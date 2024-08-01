e4c5f55b11878402f49d54a68c95254de16e51e4
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-07-31 20:31:27 -0400

    Updated typed text

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -21,10 +21,10 @@ export const HomePage: React.FC<{ blogPosts: BlogPostData[] }> = ({ blogPosts })
 
     useEffect(() => {
         const typed = new Typed(typedRef.current, {
-            strings: ['business', 'bars', 'gyms', 'restaurants', 'everyone.'],
-            startDelay: 300,
-            typeSpeed: 100,
-            backSpeed: 100,
+            strings: ['charge offs', 'collections', 'late payments', 'inquiries', 'bankruptcies', 'negative items.'],
+            startDelay: 50,
+            typeSpeed: 50,
+            backSpeed: 50,
             backDelay: 100
         });
 
@@ -45,7 +45,7 @@ export const HomePage: React.FC<{ blogPosts: BlogPostData[] }> = ({ blogPosts })
                     <div className="absolute h-[700px] md:h-[600px] w-full -mt-20 overscroll-none hero-border-radius bg-hero-gradient-background "></div>
                     <div className="relative top-0 z-20 mx-auto md:mt-24 max-w-3xl md:px-4 text-center">
                         <div className="grid grid-cols-1 text-white place-content-center justify-items-center w-full">
-                            <h1 className="text-[45px] lg:text-4xl font-bold mt-10 mb-6 max-sm:px-5">I build websites for <span ref={typedRef} /></h1>
+                            <h1 className="text-[25px] lg:text-4xl font-bold mt-10 mb-6 max-sm:px-5">I will dispute and remove <br /> <span ref={typedRef} /></h1>
                             <p className="px-4 text-lg md:w-3/4 leading-relaxed">
                                 I create lightning-fast websites to attract customers and make a great first impression. My sites are lightweight, responsive,
                                 and designed with SEO, UI, and UX in mind. Experience speed, beauty, and functionality.
