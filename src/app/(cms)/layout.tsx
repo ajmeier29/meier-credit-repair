9d0b458ab9006ba83d3640357b16dd36fada4c1d
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-08-02 23:37:07 -0400

    Got outstatic working

--- src/app/(cms)/layout.tsx
+++ src/app/(cms)/layout.tsx
@@ -1,13 +1,11 @@
-
-
 export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return (
-        <html lang="en">
-            <body>{children}</body>
-        </html>
-    )
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
 }
--- src/app/(cms)/outstatic/[[...ost]]/page.tsx
+++ src/app/(cms)/outstatic/[[...ost]]/page.tsx
@@ -3,6 +3,6 @@ import { Outstatic } from 'outstatic'
 import { OstClient } from 'outstatic/client'
 
 export default async function Page({ params }: { params: { ost: string[] } }) {
-    const ostData = await Outstatic()
-    return <OstClient ostData={ostData} params={params} />
+  const ostData = await Outstatic()
+  return <OstClient ostData={ostData} params={params} />
 }
