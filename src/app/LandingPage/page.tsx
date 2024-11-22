f9d95d1a35deb004704d2ea5bf7f603f56fceef0
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-11-21 21:17:27 -0500

    Stubbed out landing page

---
+++ src/app/LandingPage/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import AboutPage from "@/components/About";
+import CalendlyModal from "@/components/CalendlyEmbedded";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import { track } from "@vercel/analytics";
+
+import { Metadata } from "next";
+
+export default function Page() {
+    const searchParams = useSearchParams();
+    const [source, setSource] = useState<string | null>(null);
+
+    useEffect(() => {
+        const sourceParam = searchParams.get("source");
+        if (sourceParam) {
+            setSource(sourceParam); // Set the source to state for rendering
+            trackCustomEvent(sourceParam); // Track the custom event
+        }
+    }, [searchParams]);
+
+    const trackCustomEvent = (source: string) => {
+        track("qr_scan", {
+            source: source,
+            // timestamp: new Date().toISOString(),
+        });
+    };
+
+    return (
+        <>
+            <div className="grid grid-cols-1 h-[700px] md:h-[750px] w-full overscroll-none hero-border-radius bg-hero-gradient-background mb-[900px] md:mb-[250px]">
+                <Header />
+                <div className="p-4 text-center mb-[1000px]">
+                    {/* Display the source */}
+                    {source ? (
+                        <p className="text-lg font-semibold text-white">
+                            Scanned from source: <span className="text-orange-400">{source}</span>
+                        </p>
+                    ) : (
+                        <p className="text-lg">No source detected</p>
+                    )}
+                </div>
+                <Footer />
+            </div>
+            <CalendlyModal url="https://calendly.com/ajmeier29/initial-consultation" />
+        </>
+    );
+}
