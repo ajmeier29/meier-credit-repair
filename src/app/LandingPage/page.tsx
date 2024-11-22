1d0d399a51533089a750acd367140e36d29e51c8
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-11-21 21:31:25 -0500

    second attempt to fix

--- src/app/LandingPage/page.tsx
+++ src/app/LandingPage/page.tsx
@@ -7,6 +7,9 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { track } from "@vercel/analytics";
 
+// Mark the page as dynamic to prevent build-time prerendering
+export const dynamic = "force-dynamic";
+
 export default function Page() {
     const searchParams = useSearchParams();
     const [source, setSource] = useState<string | null>(null);
@@ -22,7 +25,6 @@ export default function Page() {
     const trackCustomEvent = (source: string) => {
         track("qr_scan", {
             source: source,
-            // timestamp: new Date().toISOString(),
         });
     };
 
@@ -31,7 +33,6 @@ export default function Page() {
             <div className="grid grid-cols-1 h-[700px] md:h-[750px] w-full overscroll-none hero-border-radius bg-hero-gradient-background mb-[900px] md:mb-[250px]">
                 <Header />
                 <div className="p-4 text-center mb-[1000px]">
-                    {/* Display the source */}
                     {source ? (
                         <p className="text-lg font-semibold text-white">
                             Scanned from source: <span className="text-orange-400">{source}</span>
